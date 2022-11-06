e46bef3735a5f2fb71fd52daaaff2f64afeaed66
Alina Potapchuk <alinapotapchuk7@gmail.com>
2022-11-06 14:37:47 +0300

    с анимацией самолета и адаптивом под него

---
+++ blocks/js/script.js
@@ -0,0 +1,5 @@
+let el = document.getElementById("go-anime");
+document.getElementsByClassName("travel__image")[0].addEventListener('click',
+  function () {
+    el.classList.toggle("go-animation");
+  })
